53d04ddd1351d89d83ec355e39bd7e914e739c5e
Timmy Garrabrant <timmy.garrabrant@gmail.com>
2026-04-03 08:47:33 -0700

    feat: add report issue button to header

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -46,6 +46,17 @@ export function Header({ project, user, branch, pr }: HeaderProps) {
           )}
         </div>
       </div>
+      <a
+        href="https://github.com/luigiinred/agentdashboard/issues/new"
+        target="_blank"
+        rel="noopener"
+        className="report-issue-btn"
+        title="Report an issue"
+      >
+        <svg width="16" height="16" viewBox="0 0 16 16" fill="currentColor">
+          <path d="M8 1.5a6.5 6.5 0 100 13 6.5 6.5 0 000-13zM0 8a8 8 0 1116 0A8 8 0 010 8zm9 3a1 1 0 11-2 0 1 1 0 012 0zm-.25-6.25a.75.75 0 00-1.5 0v3.5a.75.75 0 001.5 0v-3.5z"/>
+        </svg>
+      </a>
     </header>
   );
 }
